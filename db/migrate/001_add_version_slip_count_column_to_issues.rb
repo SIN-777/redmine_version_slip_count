ff727c8b4aacfced38e644d29bc80d46fa91c11e
SIN-777 <gamblemaster@gmail.com>
2013-02-01 16:37:20 +0900

    fix migration

--- db/migrate/001_add_version_slip_count_column_to_issues.rb
+++ db/migrate/001_add_version_slip_count_column_to_issues.rb
@@ -1,10 +1,12 @@
 class AddVersionSlipCountColumnToIssues < ActiveRecord::Migration
   def self.up
     add_column :issues, :version_slip_count, :integer, :null => false, :default => 0
+    Issue.record_timestamps = false
     Issue.find(:all).each do |issue|
       count = issue.get_version_slip_count_by_journals
       issue.update_attributes :version_slip_count => count
     end
+    Issue.record_timestamps = true
   end
 
   def self.down
